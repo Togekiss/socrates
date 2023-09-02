fe456b93784b06d6ca07bf9b6257f9087c41b521
Marta Zapatero <marta.zapatero@nicepeopleatwork.com>
2023-09-02 13:27:10 +0200

    download channels

--- download_channels.py
+++ download_channels.py
@@ -25,18 +25,8 @@ def download_scenes():
     with open(dm_channel_list, "r", encoding="utf-8") as file:
         json_data = json.load(file)
     
-    """ 
-    # Iterate through each object and download the channel
-    for item in json_data:
-
-        # Call the CLI command and capture its output
-        cli_command = f'dotnet DCE/DiscordChatExporter.Cli.dll export --parallel 3 -c {item["id"]} -t {c.BOT_TOKEN} -f Json -o "{c.SERVER_NAME}/Scenes/%T/%C.json" --fuck-russia'
-        output = subprocess.check_output(cli_command, shell=True, text=True)
-        print(output)
-    """
-
-    # Iterate through each object and download the channel
-    group_size = 3
+    # Iterate through the list and download channels
+    group_size = 4
     for i in range(0, len(json_data), group_size):
         group = json_data[i:i + group_size]
 
@@ -45,10 +35,25 @@ def download_scenes():
             channel_ids = channel_ids + " " + item["id"]
 
         # Call the CLI command and capture its output
-        cli_command = f'dotnet DCE/DiscordChatExporter.Cli.dll export --parallel 3 -c {channel_ids} -t {c.BOT_TOKEN} -f Json -o "{c.SERVER_NAME}/Scenes/%T/%C.json" --fuck-russia'
+        cli_command = f'dotnet DCE/DiscordChatExporter.Cli.dll export --parallel 4 -c {channel_ids} -t {c.BOT_TOKEN} -f Json -o "{c.SERVER_NAME}/Scenes/%T/%C.json" --fuck-russia'
         output = subprocess.check_output(cli_command, shell=True, text=True)
         print(output)
 
+def download_threads():
+
+    dm_channel_list = "res/thread_channel_list.json"
+    with open(dm_channel_list, "r", encoding="utf-8") as file:
+        json_data = json.load(file)
+    
+    # Iterate through each object and download the channel
+    for item in json_data:
+
+        category = item["category"].replace(":", "_")
+
+        # Call the CLI command and capture its output
+        cli_command = f'dotnet DCE/DiscordChatExporter.Cli.dll export -c {item["id"]} -t {c.BOT_TOKEN} -f Json -o "{c.SERVER_NAME}/Scenes/{category}/Threads/%C.json" --fuck-russia'
+        output = subprocess.check_output(cli_command, shell=True, text=True)
+        print(output)
 
 def download_channels():
     
@@ -56,8 +61,9 @@ def download_channels():
     get_channel_list()
 
     # run thru channel list to download it all
-    #download_DMs()
+    download_DMs()
     download_scenes()
+    download_threads()
 
     
 # invoke id_assigner.py
