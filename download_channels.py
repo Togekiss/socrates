449e6ee31ff52363b75c87456108adc51d1d87ca
Marta Zapatero <marta.zapatero@nicepeopleatwork.com>
2023-09-28 16:14:12 +0200

    paralel DMs

--- download_channels.py
+++ download_channels.py
@@ -45,13 +45,20 @@ def download_DMs():
     with open(dm_channel_list, "r", encoding="utf-8") as file:
         json_data = json.load(file)
     
-    # Iterate through each object and download the channel
-    for item in json_data:
+    # Iterate through the list and download channels
+    group_size = 3
+    for i in range(0, len(json_data), group_size):
+        group = json_data[i:i + group_size]
+
+        channel_ids = ""
+        for item in group:
+            channel_ids = channel_ids + " " + item["id"]
 
         # Call the CLI command and capture its output
-        cli_command = f'dotnet DCE/DiscordChatExporter.Cli.dll export -c {item["id"]} -t {c.BOT_TOKEN} -f Json -o "{c.SERVER_NAME}/DMs/%C.json"  --dateformat "dd/MM/yyyy HH:mm" --fuck-russia'
+        cli_command = f'dotnet DCE/DiscordChatExporter.Cli.dll export --parallel 4 -c {channel_ids} -t {c.BOT_TOKEN} -f Json -o "{c.SERVER_NAME}/DMs/%C.json" --dateformat "dd/MM/yyyy HH:mm" --fuck-russia'
         output = subprocess.check_output(cli_command, shell=True, text=True)
         print(output)
+
 
 def download_scenes():
 
@@ -92,7 +99,7 @@ def download_threads():
 def download_channels():
     
     # get the list of channels to download
-    #get_channel_list()
+    get_channel_list()
 
     # check if there's already a backup
     date = get_last_exported()
@@ -100,9 +107,9 @@ def download_channels():
     after = set_day_before(date)
 
     # run thru channel list to download it all
-    #download_DMs()
-    #download_scenes()
-    #download_threads()
+    download_DMs()
+    download_scenes()
+    download_threads()
 
     
 # invoke id_assigner.py
